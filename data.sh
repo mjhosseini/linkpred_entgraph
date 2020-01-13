414d6cd2c2996e74923f06ecf9a04df8ff1f0c63
mjhosseini <hosseini.mohammadjavad@gmail.com>
2020-01-13 19:51:01 +0000

    adding bash scripts

---
+++ data.sh
@@ -0,0 +1,13 @@
+root=convE/data
+cd $root
+wget https://www.dropbox.com/s/l5jo5bt4ueu7q12/NS.tar.gz
+tar -xvzf NS.tar.gz
+cp -r NS NS_probs_train
+cat NS_probs_train/valid.txt NS_probs_train/test.txt > NS_probs_train/valid1.txt
+mv NS_probs_train/valid1.txt NS_probs_train/valid.txt
+cp NS_probs_train/train.txt NS_probs_train/test.txt
+cp -r NS NS_probs_all
+cp NS_probs_all/all.txt NS_probs_all/test.txt
+python convE/wrangle_KG.py NS
+python convE/wrangle_KG.py NS_probs_train
+python convE/wrangle_KG.py NS_probs_all
---
+++ requirements.sh
@@ -0,0 +1,11 @@
+pip install scipy
+pip install torch
+pip install h5py
+pip install future
+pip install spacy
+pip install sklearn
+python -m spacy download en
+pip install nltk
+python -m nltk.downloader wordnet
+python -m nltk.downloader verbnet
+python -m nltk.downloader stopwords
